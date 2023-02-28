7ccf46392938549db04c74f46514c25f6e3b8bae
Dennis Murage <dmurage140@gmail.com>
2023-02-28 12:12:31 +0300

    Error handle

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,6 +9,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new, *temp;
 
+	if (!parent)
+		return (NULL);
+
 	if (parent->left == NULL)
 	{
 		new = binary_tree_node(parent->left, value);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,6 +9,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new, *temp;
 
+	if (!parent)
+		return (NULL);
+
 	if (parent->right == NULL)
 	{
 		new = binary_tree_node(parent->right, value);
