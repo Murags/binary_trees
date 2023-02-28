dbd22e2dbd3f5cdb18f4ac3fbe818227c81563b3
Dennis Murage <dmurage140@gmail.com>
2023-02-28 11:24:54 +0300

    Fixed betty styling

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_depth - measure tree depth
+ * @tree: tree to measure
+ * Return: depth
+ */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,16 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_height - measures tree height
+ * @tree: tree to measure
+ * Return: tree height
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_l;
 	size_t height_r;
+
+	if (!tree)
+		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
