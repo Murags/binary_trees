cc79484550b8d0410687b2395219e3892089b866
Dennis Murage <dmurage140@gmail.com>
2023-03-01 15:01:29 +0300

    Error handle

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,6 +10,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
+	if (!node->parent->left || !node->parent->right)
+		return (NULL);
+
 	if (node->parent->left == node && node->parent->right)
 		return (node->parent->right);
 	else if (node->parent->left)
